e78688480c11e70d12656fae620b1b89765385ae
Ali Mousazada <mousazada.ali@gmail.com>
2025-09-12 23:06:12 -0400

    Initial commit

---
+++ src/main/java/com/developeWtihAli/BooklyshopApp/BooklyshopAppApplication.java
@@ -0,0 +1,13 @@
+package com.developeWtihAli.BooklyshopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklyshopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklyshopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developeWtihAli/BooklyshopApp/BooklyshopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developeWtihAli.BooklyshopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklyshopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
